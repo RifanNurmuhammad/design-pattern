94d03bc15133b6e0e769868a514b8129126cdb6f
Rifan Nur Muhammad <Rifan.rpl@gmail.com>
2022-09-01 18:16:59 +0700

    Revert "onprogress"
    This reverts commit 4b78b4006f358e856f6600dcd1f53a4f8b89e7a5.

--- behavioral_pattern/visitor/bengkelresmi/bengkelresmi.go
+++ behavioral_pattern/visitor/bengkelresmi/bengkelresmi.go
@@ -1,31 +1,31 @@
 package bengkelresmi
 
-// import (
-// 	"fmt"
-// )
+import (
+	"fmt"
+)
 
-// type MontirOli struct{}
+type MontirOli struct{}
 
-// func (mo MontirOli) DoForGantiOli(p visitor.pelanggan) {
+func (mo MontirOli) DoForGantiOli(p visitor.pelanggan) {
 
-// }
+}
 
-// func (mo MontirOli) DoForServisRingan(p pelanggan) {
+func (mo MontirOli) DoForServisRingan(p pelanggan) {
 
-// }
+}
 
-// func (mo MontirOli) GetJenisKelamin() JK {
-// 	return Laki
-// }
+func (mo MontirOli) GetJenisKelamin() JK {
+	return Laki
+}
 
-// type PelangganOli struct {
-// 	JenisKelaminPreference JK
-// }
+type PelangganOli struct {
+	JenisKelaminPreference JK
+}
 
-// func (po PelangganOli) Setuju(m Montir) {
-// 	if m.GetJenisKelamin() == po.JenisKelaminPreference {
-// 		fmt.Println("ganti oli saja")
-// 	} else {
-// 		fmt.Println("lanjut servis ringan")
-// 	}
-// }
+func (po PelangganOli) Setuju(m Montir) {
+	if m.GetJenisKelamin() == po.JenisKelaminPreference {
+		fmt.Println("ganti oli saja")
+	} else {
+		fmt.Println("lanjut servis ringan")
+	}
+}
