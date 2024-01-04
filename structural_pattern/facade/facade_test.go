62d3cb6d51b4fe1816fb2c41fb061d69e7662d78
rifan2 <rifan@sampingan.co.id>
2024-01-04 22:17:18 +0700

    test coverage

--- structural_pattern/facade/facade_test.go
+++ structural_pattern/facade/facade_test.go
@@ -9,3 +9,13 @@ func TestTakeOrder(t *testing.T) {
 	data := Order{PaymentType: "bank", Amount: 2000}
 	fmt.Println(TakeOrder(data))
 }
+
+func TestTakeOrderEwallet(t *testing.T) {
+	data := Order{PaymentType: "mobileWallet", Amount: 2000}
+	fmt.Println(TakeOrder(data))
+}
+
+func TestTakeOrderCOD(t *testing.T) {
+	data := Order{PaymentType: "cod", Amount: 2000}
+	fmt.Println(TakeOrder(data))
+}
